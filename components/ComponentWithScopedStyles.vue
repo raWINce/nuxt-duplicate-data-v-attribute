f4f41e74f363b5f0082068e6f850b1ed96735d7e
Raoul Venn <r.venn@snoopmedia.com>
2019-09-12 11:17:53 +0200

    Make naming more readable

--- components/ComponentWithScopedStyles.vue
+++ components/ComponentWithScopedStyles.vue
@@ -2,33 +2,33 @@
     <div class="module">
         <h1>Component with render function</h1>
         <component-with-render-function :enable="false">
-          <component-with-slot>
+          <component-with-slot-and-scoped-styles>
               <div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eveniet ipsam esse porro maxime illum molestias a? Itaque impedit sint dolorum, architecto, necessitatibus nesciunt, ad, quisquam numquam debitis quidem nulla!</p>
               </div>
-          </component-with-slot>
+          </component-with-slot-and-scoped-styles>
         </component-with-render-function>
 
         <hr>
         <h2>Duplicate data-v attribute</h2>
         <component-with-render-function enable>
-          <component-with-slot>
+          <component-with-slot-and-scoped-styles>
               <div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eveniet ipsam esse porro maxime illum molestias a? Itaque impedit sint dolorum, architecto, necessitatibus nesciunt, ad, quisquam numquam debitis quidem nulla!</p>
               </div>
-          </component-with-slot>
+          </component-with-slot-and-scoped-styles>
         </component-with-render-function>
     </div>
 </template>
 
 <script>
     import ComponentWithRenderFunction from '~/components/ComponentWithRenderFunction'
-    import ComponentWithSlot from './ComponentWithSlot'
+    import ComponentWithSlotAndScopedStyles from './ComponentWithSlot'
 
     export default {
       components: {
         ComponentWithRenderFunction,
-        ComponentWithSlot,
+        ComponentWithSlotAndScopedStyles,
       }
     }
 </script>
